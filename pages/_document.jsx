2dce9e684c23da8d144bd2acb7b8b58acdeb8a66
Madmoon <phosuphong.j@kkumail.com>
2024-11-20 23:06:47 +0700

    add more misc

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,49 +8,6 @@ export default function Document() {
       lang="en"
     >
       <Head>
-        {/* <title>pranjal ✦ developer, designer</title> */}
-        <meta name="title" content="pranjal ✦ developer, designer" />
-        <meta name="description" content="Developer // Designer // Weeb" />
-
-        {/* <!-- Open Graph / Facebook --> */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://pranjalshikhar.vercel.app/" />
-        <meta property="og:title" content="pranjal ✦ developer, designer" />
-        <meta
-          property="og:description"
-          content="Developer // Designer // Weeb"
-        />
-        <meta
-          property="og:image"
-          content="https://res.cloudinary.com/dhtviyhse/image/upload/v1682600618/pranjalshikhar_i8znzx.jpg"
-        />
-
-        {/* <!-- Twitter --> */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:url"
-          content="https://pranjalshikhar.vercel.app/"
-        />
-        <meta
-          property="twitter:title"
-          content="pranjal ✦ developer, designer"
-        />
-        <meta
-          property="twitter:description"
-          content="Developer // Designer // Weeb"
-        />
-        <meta
-          property="twitter:image"
-          content="https://res.cloudinary.com/dhtviyhse/image/upload/v1682600618/pranjalshikhar_i8znzx.jpg"
-        />
-
-        {/* whatsapp */}
-        <meta property="og:site_name" content="pranjal ✦ developer, designer" />
-        <meta property="og:title" content="pranjal ✦ developer, designer" />
-        <meta
-          property="og:description"
-          content="Developer // Designer // Weeb"
-        />
         <meta
           property="og:image"
           itemprop="image"
